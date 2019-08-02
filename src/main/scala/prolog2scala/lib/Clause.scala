f9eee4b4496b5fd33e8c7f5e802f4580471e9365
luca.tremamunno <luca.tremamunno@studio.unibo.it>
2019-08-02 16:04:00 +0200

    Added type variance

--- src/main/scala/prolog2scala/lib/Clause.scala
+++ src/main/scala/prolog2scala/lib/Clause.scala
@@ -3,21 +3,21 @@ package prolog2scala.lib
 import prolog2scala.lib.Clause.Solution.{CutSolution, SimpleSolution}
 import prolog2scala.lib.Clause.Solution
 
-sealed trait Clause[A,B] {
+sealed trait Clause[-A,+B] {
   def apply(arg: A): Solution[B]
 }
 
 object Clause {
-  sealed trait Solution[A] {
+  sealed trait Solution[+A] {
     def solutionsLazyList: LazyList[A]
   }
 
   object Solution {
-    case class SimpleSolution[A](override val solutionsLazyList: LazyList[A]) extends Solution[A]
-    case class CutSolution[A](override val solutionsLazyList: LazyList[A]) extends Solution[A]
+    case class SimpleSolution[+A](override val solutionsLazyList: LazyList[A]) extends Solution[A]
+    case class CutSolution[+A](override val solutionsLazyList: LazyList[A]) extends Solution[A]
   }
 
-  private class Rule[A, B](body: PartialFunction[A, Solution[B]]) extends Clause[A,B] {
+  private class Rule[-A,+B](body: PartialFunction[A, Solution[B]]) extends Clause[A,B] {
     override def apply(arg: A): Solution[B] = body.applyOrElse(arg, (_: A) => SimpleSolution(LazyList()))
   }
 
@@ -30,7 +30,7 @@ object Clause {
       new Rule(body.andThen(res => if (res.didCut) CutSolution(res.solutions) else SimpleSolution(res.solutions)))
   }
 
-  case class Fact[A, B](body: PartialFunction[A, B]) extends Clause[A,B] {
+  case class Fact[-A, +B](body: PartialFunction[A, B]) extends Clause[A,B] {
     override def apply(arg: A): Solution[B] = SimpleSolution(body.andThen(LazyList(_)).applyOrElse(arg, (_: A) => LazyList()))
   }
 }
--- src/main/scala/prolog2scala/lib/Predicate.scala
+++ src/main/scala/prolog2scala/lib/Predicate.scala
@@ -3,7 +3,7 @@ package prolog2scala.lib
 import prolog2scala.lib.Clause.Solution.{CutSolution, SimpleSolution}
 import prolog2scala.lib.Implicits._
 
-case class Predicate[A,B](clauses: Clause[A,B]*) {
+case class Predicate[-A,+B](clauses: Clause[A,B]*) {
   def apply(arg: A): LazyList[B] =
     clauses to LazyList map {
       _(arg)
